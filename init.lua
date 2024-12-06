22b1eef944eaab5b3f66cbb2aa03296a867a5377
Ivan Vostrilov <vnstr.main@gmail.com>
2024-12-06 19:43:31 +0300

    Base configuration. Includes: plugin manager, colorscheme, status-line, file-tree, fuzzy-finder

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- Plugin manager. Must be first.
+require("config.lazy")
+
+-- Core nvim config
+require("config.nvim")
---
+++ lua/plugins/status-line.lua
@@ -0,0 +1,40 @@
+-- https://github.com/sontungexpt/sttusline
+
+return {
+  "sontungexpt/sttusline",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  event = { "BufEnter" },
+  opts = {
+    -- statusline_color = "#000000",
+    statusline_color = "StatusLine",
+
+    -- | 1 | 2 | 3
+    -- recommended: 3
+    laststatus = 3,
+    disabled = {
+      filetypes = {
+        -- "NvimTree",
+        -- "lazy",
+      },
+      buftypes = {
+        -- "terminal",
+      },
+    },
+    components = {
+      "mode",
+      "filename",
+      "git-branch",
+      "git-diff",
+      "%=",
+      "diagnostics",
+      "lsps-formatters",
+      "copilot",
+      "indent",
+      "encoding",
+      "pos-cursor",
+      "pos-cursor-progress",
+    },
+  }
+}
