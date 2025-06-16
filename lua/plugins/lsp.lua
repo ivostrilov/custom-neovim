9399979886225270f91568a1749928d38643d742
Ivan Vostrilov <vnstr.main@gmail.com>
2025-06-16 20:00:50 +0300

    Format python files by cmd commands

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -29,12 +29,11 @@ return {
           pylsp = {
             plugins = {
               -- formatter
-              black = { enabled = true, path = "poetry run pyflakes" },
-
-              isort = { enabled = true }, -- sorting import
+              black = { enabled = true, line_length = 80 },
+              isort = { enabled = true, line_length = 80, profile = "black" },
 
               -- static analysis
-              pyflakes = { enabled = true, path = "poetry run pyflakes" },
+              pyflakes = { enabled = true },
               flake8 = { enabled = true },
 
               -- отключаем конфликтующие плагины
@@ -78,16 +77,45 @@ return {
 
     -- Editing commands
     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { desc = "LSP rename under cursor" })
-    vim.keymap.set("n", "<space>rf", function()
-      vim.lsp.buf.format({ async = false })
 
-      -- Выполняем isort только для Python-файлов
-      local filetype = vim.bo.filetype
-      if filetype == "python" and vim.fn.expand("%") ~= "" then
-        vim.cmd(":w")  -- Сохраняем файл
-        vim.cmd(":silent !poetry run isort --quiet %")
-        vim.cmd(":edit!")  -- Перезагружаем буфер
+    vim.keymap.set("n", "<space>rf", function()
+      if vim.bo.filetype == "python" then
+        vim.cmd(":w")
+
+        local filepath = vim.fn.shellescape(vim.fn.expand("%:p"))
+
+        local black_cmd = string.format("black --line-length 79 --quiet %s", filepath)
+        local isort_cmd = string.format("isort --line-length 79 --profile black --quiet %s", filepath)
+
+        local function execute(cmd, name)
+          local output = vim.fn.system(cmd)
+          if vim.v.shell_error ~= 0 then
+            vim.notify(string.format("%s failed: %s", name, output), vim.log.levels.ERROR)
+            return false
+          end
+          return true
+        end
+
+        local black_success = execute(black_cmd, "Black")
+
+        if not black_success then
+          table.insert(notify_messages, "Black formatting failed")
+          return
+        end
+
+        local isort_success = execute(isort_cmd, "Isort")
+
+        if not isort_success then
+          table.insert(notify_messages, "isort failed")
+          return
+        end
+
+        vim.cmd(":e!")
+
+      else
+        vim.lsp.buf.format({ async = false })
       end
     end, { desc = "Format & sort imports" })
+
   end
 }
